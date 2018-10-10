85ec1b5fc65e2a5d5efee59b15bd132074e56e42
kizmo04 <get.kizmo@gmail.com>
2018-10-10 13:12:54 +0900

    API - POST User login/signup endpoint : install cors

--- app.js
+++ app.js
@@ -1,30 +1,32 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var mongoose = require("mongoose");
-const config = require('./config');
-const bodyParser = require('body-parser');
+const cors = require("cors");
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+const config = require("./config");
+const bodyParser = require("body-parser");
 
 mongoose.connect(
   config.mongoDB,
-  {useNewUrlParser: true}
+  { useNewUrlParser: true }
 );
-mongoose.set('useCreateIndex', true);
+mongoose.set("useCreateIndex", true);
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const indexRouter = require("./routes/index");
+const usersRouter = require("./routes/users");
 
-var app = express();
+const app = express();
 
-app.use(logger('dev'));
+app.use(cors());
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/api', indexRouter);
-app.use('/api/users', usersRouter);
+app.use("/api", indexRouter);
+app.use("/api/users", usersRouter);
 
 app.use(function(err, req, res, next) {
   res.status(err.status).json({ message: err.message });
