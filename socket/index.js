d92595929cf9cab6b1b7e95304e7750cf6a4b6a1
kizmo04 <get.kizmo@gmail.com>
2018-10-13 00:42:58 +0900

    [API] Find user match - constants

--- socket/index.js
+++ socket/index.js
@@ -1,8 +1,19 @@
 const _ = require('lodash');
 const uuidv4 = require('uuid/v4');
+const {
+  CONNECTION,
+  USER_LOGIN,
+  USER_LOGOUT,
+  USER_DISCONNECTION,
+  REFUSE,
+  JOIN_OPPONENT,
+  ACCEPTANCE,
+  NO_OPPONENT,
+  WAITING_TO_ACCEPT,
+  INVITE,
+  REQUEST_OPPONENT,
+} = require('../constants/socketEventTypes');
 var onUsers = [];
-
-//랜덤 유저 뽑는 함수 유틸로 따로 뽑기
 
 function getRandomClient(users, hashString) {
   const randomIndex = parseInt((parseInt(hashString) || hashString.charCodeAt(0) || hashString.length) % users.length);
@@ -10,93 +21,60 @@ function getRandomClient(users, hashString) {
 }
 
 module.exports = io => {
-  console.log('socket connect, current on users: \n', onUsers, '\n');
-  console.log('\n전체 rooms: \n', io.of('/').adapter.rooms, '\n 클라이언트의 수: \n' + Object.keys(io.of('/').adapter.rooms).length + '\n');
-
-  io.of('/').on('connection', socket => {
-    socket.on('login user', user => {
+  io.of('/').on(CONNECTION, socket => {
+    socket.on(USER_LOGIN, user => {
       if (Object.keys(user).length) {
         onUsers.push({
           socketId: socket.id,
           user
         });
       }
-      console.log(`log in user ${_.find(onUsers, client => client.socketId === socket.id).user.email}`);
-      console.log('current on users: \n', onUsers, '\n');
-      console.log('\n전체 rooms: \n', io.of('/').adapter.rooms, '\n 클라이언트의 수: \n' + Object.keys(io.of('/').adapter.rooms).length + '\n');
     });
 
-    socket.on('logout user', () => {
-      console.log('log out user')
+    socket.on(USER_LOGOUT, () => {
       if (onUsers.map(client => client.socketId).includes(socket.id)) {
         onUsers.splice(_.findIndex(onUsers, client => client.socketId === socket.id), 1);
       }
-      // console.log(`log out user ${Object.keys(_.find(onUsers, client => client.socketId === socket.id)).length ? _.find(onUsers, client => client.socketId === socket.id).user.email : null}`);
-      console.log('current on users: \n', onUsers, '\n');
-      console.log('\n전체 rooms: \n', io.of('/').adapter.rooms, '\n 클라이언트의 수: \n' + Object.keys(io.of('/').adapter.rooms).length + '\n');
     });
 
-    socket.on('disconnect', data => {
-      console.log('disconnect user \n' + socket.id)
-      // console.log(`disconnect user ${Object.keys(_.find(onUsers, client => client.socketId === socket.id)).length ? _.find(onUsers, client => client.socketId === socket.id).user.email : null}`);
+    socket.on(USER_DISCONNECTION, data => {
       if (onUsers.map(client => client.socketId).includes(socket.id)) {
         onUsers.splice(_.findIndex(onUsers, client => client.socketId === socket.id), 1);
       }
-      console.log('current on users: \n', onUsers, '\n');
-      console.log('\n전체 rooms: \n', io.of('/').adapter.rooms, '\n 클라이언트의 수: \n' + Object.keys(io.of('/').adapter.rooms).length + '\n');
     });
 
-    socket.on('find someone to match', user => {
+    socket.on(REQUEST_OPPONENT, (hostUser, prevCombatRoomKey) => {
       const hostUserIndex = _.findIndex(onUsers, client => client.socketId === socket.id);
-      console.log(`${onUsers[hostUserIndex].user.email} find someone to combat`);
-      onUsers.splice(hostUserIndex, 1);
+
+      if (prevCombatRoomKey) socket.leave(prevCombatRoomKey);
+      if (hostUserIndex > -1) onUsers.splice(hostUserIndex, 1);
 
       const combatRoomKey = uuidv4();
       socket.join(combatRoomKey);
       socket.leave(socket.id);
-      socket.to(combatRoomKey).emit('waiting for player');
 
       if (onUsers.length) {
-        console.log('request user info to clients')
-        socket.broadcast.emit('requst user info', { hostSocketId: socket.id, hostUser: user, combatRoomKey });
+        const randomClient = getRandomClient(onUsers, combatRoomKey);
+        const guestUser = randomClient.user;
+        const guestSocketId = randomClient.socketId;
+        io.of('/').to(guestSocketId).emit(INVITE, { hostUser, combatRoomKey });
+        io.of('/').to(combatRoomKey).emit(WAITING_TO_ACCEPT, { guestUser });
       } else {
-        io.of('/').to(combatRoomKey).emit('no one is here');
-        console.log('no one is here');
+        io.of('/').to(combatRoomKey).emit(NO_OPPONENT);
         socket.leave(combatRoomKey);
         socket.join(socket.id);
       }
-      console.log('current on users: \n' + onUsers + '\n');
-      console.log('\n전체 rooms: \n', io.of('/').adapter.rooms, '\n 클라이언트의 수: \n' + Object.keys(io.of('/').adapter.rooms).length + '\n');
     });
 
-    socket.on('response user info', ({hostUser, combatRoomKey, guestSocketId, hostSocketId }) => {
-      console.log('current on users: \n', onUsers, '\n');
-      console.log('\n전체 rooms: \n', io.of('/').adapter.rooms, '\n 클라이언트의 수: \n' + Object.keys(io.of('/').adapter.rooms).length + '\n');
-
-      const randomClient = getRandomClient(onUsers, combatRoomKey);
-
-      if (randomClient.socketId === guestSocketId) {
-        const guestUser = randomClient.user;
-        console.log(`response from client and pick randomly ${guestUser.email}`);
-        io.of('/').to(guestSocketId).emit('will you join?', {hostUser, combatRoomKey, guestSocketId, hostSocketId, guestUser});
-        console.log(`invite ${randomClient.user.email}`);
-        io.of('/').to(combatRoomKey).emit('waiting for guest to accept', { guestUser });
-      }
-    });
-
-    socket.on('accept combat', ({hostUser, combatRoomKey, hostSocketId, guestUser}) => {
-      // console.log(`${guestUser.email} accept invitation`);
+    socket.on(ACCEPTANCE, ({ hostUser, combatRoomKey }) => {
       socket.join(combatRoomKey);
-      // socket.leave(guestSocketId);
       socket.leave(socket.id);
       onUsers.splice(_.findIndex(onUsers, client => client.socketId === socket.id), 1);
-      const combatRoom = io.of('/').adapter.rooms[combatRoomKey];
-      const participants = Object.keys(combatRoom).length;
+      io.of('/').to(combatRoomKey).emit(JOIN_OPPONENT, { hostUser, combatRoomKey });
+    });
 
-      console.log('current on users: \n', onUsers, '\n');
-      console.log('\n전체 rooms: \n', io.of('/').adapter.rooms, '\n 클라이언트의 수: \n' + Object.keys(io.of('/').adapter.rooms).length + '\n');
-      // console.log(`참여자 수 : ${participants}명, ${hostUser.email}, ${guestUser.email}`);
-      io.of('/').to(combatRoomKey).emit('join guest', {hostUser, combatRoomKey});
+    socket.on(REFUSE, ({combatRoomKey}) => {
+      io.of('/').to(combatRoomKey).emit(REFUSE, combatRoomKey);
     });
   });
 };
